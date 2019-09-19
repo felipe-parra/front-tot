d347ce2037d0b33caca8aedd3ca52ba2114db8ab
Felipe <fpr29.10@gmail.com>
2019-09-18 23:29:16 -0500

    changing antd for materialze

--- src/Routes.js
+++ src/Routes.js
@@ -1,30 +1,23 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Layout } from "antd";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Home from "./pages/Home";
-import Dashboard from "./pages/Dashboard";
-import Nav from "./components/Nav";
-import Register from "./pages/Register";
-import NotFound from "./pages/NotFound";
-
-const { Content, Footer } = Layout;
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Nav from './components/Nav';
+import Register from './pages/Register';
+import NotFound from './pages/NotFound';
 
 const Routes = () => (
-  <Router>
-    <Nav></Nav>
-    <Layout>
-      <Content>
-        <Switch>
-          <Route exact path="/" component={Home}></Route>
-          <Route path="/dashboard" component={Dashboard}></Route>
-          <Route path="/register" component={Register}></Route>
-          <Route component={NotFound}></Route>
-        </Switch>
-      </Content>
-      <Footer>footer</Footer>
-    </Layout>
-  </Router>
+	<Router>
+		<Nav />
+
+		<Switch>
+			<Route exact path="/" component={Home} />
+			<Route path="/dashboard" component={Dashboard} />
+			<Route path="/register" component={Register} />
+			<Route component={NotFound} />
+		</Switch>
+	</Router>
 );
 
 export default Routes;
